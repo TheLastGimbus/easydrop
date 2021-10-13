4b4ea512d93104a9e0e760d3e8c492a709840fae
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-10-13 23:15:35 +0200

    More network lifecycle

--- easydrop/cli.py
+++ easydrop/cli.py
@@ -1,7 +1,13 @@
+import sys
+
 import click
 from loguru import logger
+from opendrop import config as ad_config
 
 from .owl import Owl
+
+logger.remove()
+logger.add(sys.stdout, format='<green>{time:HH:mm:ss}</green> <level>{message}</level>', level='TRACE')
 
 
 @click.group()
@@ -11,9 +17,15 @@ def main():
 
 
 @main.command()
+@click.option('--email')
 @logger.catch
-def receive():
+def receive(email):
     with Owl() as o:
+        config = ad_config.AirDropConfig(
+            airdrop_dir='~/.config/easydrop/opendrop',
+            email=email
+        )
+        # srv = ad_server.AirDropServer(config)
         input("Press enter to stop OWL...")
 
 
--- easydrop/owl.py
+++ easydrop/owl.py
@@ -1,26 +1,50 @@
+import signal
 import subprocess
 from getpass import getpass
 
 from loguru import logger
-import signal
+
+# Order is important
+_services_to_stop = [
+    'NetworkManager',
+    'avahi-daemon',
+    'wpa_supplicant',
+]
 
 
 class Owl:
     def __inti__(self):
         pass
 
+    def _run_sudo(self, cmd):
+        subprocess.run(
+            f'sudo -S {cmd}'.split(), input=self.sudo_pwd, text=True, timeout=10)
+
     def __enter__(self):
-        sudo_pwd = getpass('sudo password for OWL: ')
+        self.sudo_pwd = getpass('sudo password: ')
         # TODO: Some password retry
+
+        logger.warning('Hang tight! Disabling normal WiFi...')
+        for s in _services_to_stop:
+            self._run_sudo(f'systemctl stop {s}')
+
+        logger.info('Starting OWL...')
         self.owl_process = subprocess.Popen(
             'sudo -S owl -i wlp1s0'.split(),
             stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        self.owl_process.communicate(input=sudo_pwd.encode())
-        logger.info("OWL started")
+        self.owl_process.communicate(input=self.sudo_pwd.encode())
+        logger.success("OWL running!")
         pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        logger.info("OWL exiting...")
+        logger.info("Stopping OWL...")
         self.owl_process.send_signal(signal.SIGSTOP)
         self.owl_process.wait(10)
+        logger.info('Restarting network...')
+        # TODO: replace hard-coded interface name
+        self._run_sudo('ifconfig wlp1s0 down')
+        self._run_sudo('iwconfig wlp1s0 mode managed')
+        self._run_sudo('ifconfig wlp1s0 up')
+        for s in reversed(_services_to_stop):
+            self._run_sudo(f'systemctl restart {s}')
         pass
